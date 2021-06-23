e1eb9509fbf8e7c00f6b65025b16d0123c54fa21
Damien Dupuy <sixty.nuuuts@gmail.com>
2021-06-23 23:55:59 +0200

    add @isGranted into Dashboards

--- src/Controller/DoctorController.php
+++ src/Controller/DoctorController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/kine")
@@ -13,6 +14,7 @@ class DoctorController extends AbstractController
 {
     /**
      * @Route("/", name="app_doctor_dashboard")
+     * @isGranted("ROLE_DOCTOR", message="Vous n'avez pas le droit d'accéder à cette zone")
      */
     public function dashboard(): Response
     {
--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/patient")
@@ -13,6 +14,7 @@ class PatientController extends AbstractController
 {
     /**
      * @Route("/", name="app_patient_dashboard")
+     * @isGranted("ROLE_PATIENT", message="Vous n'avez pas le droit d'accéder à cette zone")
      */
     public function dashboard(): Response
     {
