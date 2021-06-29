5378442f4311f517973b67156f701fdcd46ce55a
Damien Dupuy <sixty.nuuuts@gmail.com>
2021-06-29 14:06:40 +0200

    create Video & Tag Entity+AdminCRUD 2

--- src/Controller/Admin/VideoCrudController.php
+++ src/Controller/Admin/VideoCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\Video;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 
 class VideoCrudController extends AbstractCrudController
@@ -22,6 +23,7 @@ class VideoCrudController extends AbstractCrudController
             TextField::new('url'),
             ImageField::new('thumbnail_url')->onlyOnIndex(),
             TextEditorField::new('description'),
+            AssociationField::new('tags'),
         ];
     }
 }
