fcf61d5c52b35bbe0fe21bb3fd7a37c031426c2f
Damien Dupuy <sixty.nuuuts@gmail.com>
2021-06-30 18:45:44 +0200

    create Edit Action + Modal + Ajax

---
+++ src/Controller/Patient/ManageController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Patient;
+
+use App\Entity\Patient;
+use App\Form\PatientFormType;
+use App\Repository\PatientRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/patient")
+ */
+class ManageController extends AbstractController
+{
+    /**
+     * @Route("/{id}/edit", name="app_patient_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Patient $patient, SluggerInterface $slugger): Response
+    {
+        $form = $this->createForm(PatientFormType::class, $patient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $uploadedFile = $form['avatarFile']->getData();
+            if ($uploadedFile) {
+                $destination = $this->getParameter('kernel.project_dir') . '/public/uploads/avatar';
+                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = strtolower($slugger->slug($originalFilename))
+                               . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
+                $uploadedFile->move($destination, $newFilename);
+                $patient->setAvatarUrl('/uploads/avatar/' . $newFilename);
+            }
+
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', 'Votre profil a été mis à jour !');
+
+            return $this->redirectToRoute('app_patient_dashboard', ['id' => $patient->getId()]);
+        }
+
+        return $this->render("patient/modal-edit.html.twig", [
+            'patient' => $patient,
+            'form' => $form->createView(),
+        ]);
+    }
+}
--- src/Form/PatientFormType.php
+++ src/Form/PatientFormType.php
@@ -5,43 +5,41 @@ namespace App\Form;
 use App\Entity\Patient;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 
 class PatientFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('password', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'invalid_message' => 'Les champs de mot de passe doivent correspondre.',
-                'options' => ['attr' => ['class' => 'password-field']],
-                'required' => true,
-                'first_options'  => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Répéter le mot de passe'],
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'autofocus' => true,
+                    'placeholder' => "Prénom",
+                ],
             ])
-            ->add('firstname', TextType::class)
-            ->add('lastname', TextType::class)
-            ->add('avatarUrl', TextType::class)
-            ->add('birthdate', DateType::class, [
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => "Nom",
+                ],
+            ])
+            ->add('avatarFile', FileType::class, [
+                'required' => false,
+                'label' => 'Avatar',
+                'mapped' => false
+            ])
+            ->add('birthdate', BirthdayType::class, [
+                'label' => 'Date de naissance',
+                'placeholder' => [
+                    'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour',
+                ],
                 'widget' => 'choice',
                 'input'  => 'datetime_immutable'
-            ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'Vous devez accepter nos conditions.',
-                    ]),
-                ],
             ])
         ;
     }
