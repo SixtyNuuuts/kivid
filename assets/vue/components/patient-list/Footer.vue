d677561e8b3c9f0f710f1eab757338454bed4d8d
Damien Dupuy <sixty.nuuuts@gmail.com>
2021-07-03 22:39:01 +0200

    add Vuesax UI framework

--- assets/vue/components/patient-list/Footer.vue
+++ assets/vue/components/patient-list/Footer.vue
@@ -1,12 +1,25 @@
 <template>
-    <h1>Footer</h1>
+    <div class="center">
+        <vs-button :active="active == 0" @click="active = 0">Active</vs-button>
+        <vs-button :active="active == 1" @click="active = 1">Default</vs-button>
+        <vs-button disabled>Disabled</vs-button>
+    </div>
 </template>
 
 <script>
 export default {
     name: "Footer",
+    data: () => ({
+        active: 0,
+    }),
 };
 </script>
 
 <style lang="scss">
+.center {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+}
 </style>
