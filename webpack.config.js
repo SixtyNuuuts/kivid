914777efd277508dc4bd42ac7cc887f9332fafa1
Damien Dupuy <sixty.nuuuts@gmail.com>
2021-07-15 08:08:33 +0200

    KIV-23-24-26-54 correc 3.2

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ Encore
     .addEntry('my-worksheets-page', './assets/vue/doctor/my-worksheets-page.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    // .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
