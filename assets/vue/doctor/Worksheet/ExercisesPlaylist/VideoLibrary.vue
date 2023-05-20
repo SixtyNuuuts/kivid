5268c9405ad99c32b2266ca66953d1f3ba011ffc
Damien Dupuy <sixty.nuuuts@gmail.com>
2023-05-20 11:00:08 +0200

    kivid-ffmkr: modif css

--- assets/vue/doctor/Worksheet/ExercisesPlaylist/VideoLibrary.vue
+++ assets/vue/doctor/Worksheet/ExercisesPlaylist/VideoLibrary.vue
@@ -164,10 +164,9 @@
                                 >
                                     <svg v-if="video.videoLibrary.reference === 'ffmkr' && !getDoctorIsFFMKRAdherent" version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                                         viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve">
-                                    <path class="st0" d="M29.6,5.3l1.7-1.7C31.8,3.2,32,2.6,32,2.1c0-0.6-0.2-1.1-0.6-1.5c-0.8-0.8-2.1-0.8-2.9,0L11.9,17.1
-                                        c-1.2-0.6-2.5-1-3.9-1c-4.4,0-8,3.6-8,7.9S3.6,32,8,32s8-3.6,8-7.9c0-1.5-0.4-2.8-1.1-4l9.1-9.1l2.1,2.1c0.1,0.1,0.3,0.2,0.5,0.2
-                                        c0.2,0,0.3-0.1,0.5-0.2l4.8-4.7C31.9,8.3,32,8.1,32,7.9c0-0.2-0.1-0.3-0.2-0.5L29.6,5.3z M7.9,28.2c-2.4,0-4.4-1.9-4.4-4.2
-                                        c0-2.3,2-4.2,4.4-4.2s4.4,1.9,4.4,4.2C12.3,26.3,10.3,28.2,7.9,28.2z"/>
+                                        <path style="fill:none;stroke:#E7DFCD;stroke-width:2;stroke-miterlimit:10;" d="M16,8H8H6H4v14h16V8h-2H16z M12,17c-1.1,0-2-0.9-2-2s0.9-2,2-2s2,0.9,2,2S13.1,17,12,17z"/>
+                                        <path style="fill:#E7DFCD;" d="M7.5,3.3C8.6,1.9,10.3,1,12.2,1c3.3,0,6,2.7,6,6v1h-2V7c0-2.3-1.7-4-4-4C10.9,3,9.7,3.6,9,4.5L7.5,3.3z"/>
+                                        <path style="fill:none;stroke:#E7DFCD;stroke-width:2;stroke-miterlimit:10;" d="M8.2,7"/>
                                     </svg>
                                     <plus-icon v-else size="2x"></plus-icon>
                                 </vs-button>
@@ -1163,9 +1162,15 @@ export default {
                         }
 
                         &.btn-unlock-vid {
+                            .vs-button__content {
+                                padding: 2px;
+                            }
+
                            svg {
-                                width: 1.33rem;
-                                transform: rotate(43deg);
+                                width: 2.33rem;
+                                position: relative;
+                                top: 0.2rem;
+                                left: 0.3rem;
                             }
                         }
                     }
