f55a9ff316f4df8bb863fb3f24c7c45e28b866a7
Damien Dupuy <sixty.nuuuts@gmail.com>
2023-09-16 16:52:42 +0200

    axeptio script

--- assets/vue/doctor/DashboardPage.vue
+++ assets/vue/doctor/DashboardPage.vue
@@ -431,7 +431,6 @@ export default {
             var historyTraversal = event.persisted || 
             ( typeof window.performance != "undefined" && 
                 window.performance.navigation.type === 2 );
-                console.log(historyTraversal)
             if ( historyTraversal ) {
                 // Handle page restore.
                 window.location.reload();
